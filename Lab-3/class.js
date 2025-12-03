f794295aa381f184843cea9db18c479d5f44283e
Nency Parmar <nencyy@Nencys-MacBook-Air.local>
2025-12-03 19:12:41 +0530

    Added Files...

---
+++ Lab-3/class.js
@@ -0,0 +1,11 @@
+class Student {
+    constructor(n, r) {
+        this.name = n;
+        this.roll = r;
+    }
+    show() {
+        return this.name + " : " + this.roll;
+    }
+}
+let s = new Student("Nency", 12);
+document.write(s.show());
